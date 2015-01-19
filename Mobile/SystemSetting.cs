c6688dcf0ddcb1e5b5baada70986b74706e3e2ac
gsp40213 <gsp40213@gmail.com>
2015-01-19 14:40:56 +0800

    update systemsetting.cs

--- Mobile/SystemSetting.cs
+++ Mobile/SystemSetting.cs
@@ -4,6 +4,23 @@ using System.Collections;
 public class SystemSetting {
 
 	void OnGUI(){
-		GUI.Window(1,
+		setAlertDialog ();
+	}
+
+	public void setAlertDialog (){
+
+		DpiResolution dpiResolution = new DpiResolution ();
+
+		float windowsWidth = dpiResolution.getScreenWidth(), widowsHeight = dpiResolution.setScreenHeight(200f), height = dpiResolution.setScreenHeight(80f);
+
+		Debug.Log (height);
+
+		GUI.Window (0, new Rect(dpiResolution.getScreenWidth()/dpiResolution.getScreenWidth(), dpiResolution.getScreenHeight()/dpiResolution.getScreenHeight() + height,
+		                        dpiResolution.setScreenWidth(windowsWidth), dpiResolution.setScreenHeight(widowsHeight)), GameSetting, "GameSetting"); 
+			
+	}
+
+	void GameSetting(int windowID){
+
 	}
 }
