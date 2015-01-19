7bac33e963c545930a76476f5ce0b4a468d5ef13
unknown <cslag@hotmail.com.tw>
2015-01-19 15:22:49 +0800

    added get and set Method over setAlertDialog

--- Mobile/SystemSetting.cs
+++ Mobile/SystemSetting.cs
@@ -1,6 +1,9 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/**
+* 
+*/
 public class SystemSetting {
 
 	void OnGUI(){
@@ -8,16 +11,11 @@ public class SystemSetting {
 	}
 
 	public void setAlertDialog (){
-
 		DpiResolution dpiResolution = new DpiResolution ();
-
 		float windowsWidth = dpiResolution.getScreenWidth(), widowsHeight = dpiResolution.setScreenHeight(200f), height = dpiResolution.setScreenHeight(80f);
-
 		Debug.Log (height);
-
 		GUI.Window (0, new Rect(dpiResolution.getScreenWidth()/dpiResolution.getScreenWidth(), dpiResolution.getScreenHeight()/dpiResolution.getScreenHeight() + height,
 		                        dpiResolution.setScreenWidth(windowsWidth), dpiResolution.setScreenHeight(widowsHeight)), GameSetting, "GameSetting"); 
-			
 	}
 
 	void GameSetting(int windowID){
