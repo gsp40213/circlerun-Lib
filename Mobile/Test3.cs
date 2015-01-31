c8baf196e1bb104effac256ca3c9ab350f3f8c0d
gsp40213 <gsp40213@gmail.com>
2015-02-01 00:58:15 +0800

    Test3.cs

---
+++ Mobile/Test3.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test3 : MonoBehaviour{
+
+
+	SystemSetting systemSetting = new SystemSetting ();
+
+	bool sw = false;
+
+	void OnGUI(){
+
+		if(GUI.Button(new Rect(Screen.width/Screen.width, Screen.height/Screen.height, 100, 50),"aaa")){
+			sw = !sw;
+		}
+
+		if(sw){
+			systemSetting.setAlertDialog("test");
+			systemSetting.getWindowSetting ();
+		}
+	}
+	public void testWidnws(int Id){
+
+	}
+}
